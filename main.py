c6305553b589b4b0a86bed626fcb4f41cf257ead
Ryland <rylandgrounds@gmail.com>
2021-02-21 16:25:42 -0600

    initial setup

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import gspread
 import constants
 
 
-credentials = Constants.API_KEY_SERVICE
+credentials = constants.API_KEY_SERVICE
 
 
-gc = gspread.service_account(credentials)
+gc = gspread.service_account(filename='/Users/rylandgrounds/Downloads/twitter-twatter--1613940316400-060db39d0e19.json')
 
 # Open a sheet from a spreadsheet in one go
 wks = gc.open("Twitter Twatter Tracker").sheet1
 
 # Update a range of cells using the top left corner address
-wks.update('A1', [[1, 2], [3, 4]])
+wks.update('A1', [['hello', 'world'], [3, 4]])
 
 
